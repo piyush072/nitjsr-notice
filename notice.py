29d200e4700287a44939e21a32c34bf481488cdc
Piyush <2017ugcs007@nitjsr.ac.in>
2019-07-08 02:31:48 +0530

    shows notification when new notice arrives

--- notice.py
+++ notice.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 from os import path
 import urllib
 import os
+import subprocess as s
 
 URL = "http://www.nitjsr.ac.in/allnotice.php"
 
@@ -19,22 +20,23 @@ for t in soup.find_all('table',attrs={'class':'table table-striped'}):
         name.append(a.text+'\n')
         link.append(a['href'].replace(' ','%20'))
 
-if(path.exists(".notifications.txt")):
-    file = open(".notifications.txt", "r")
+if(path.exists(".notice.txt")):
+    file = open(".notice.txt", "r")
     x = file.readline()
     for i in range(0,10):
         if(x == name[i]):
             break
 st = ""
-my_path = '/home/piyush/Downloads/NITJSR_NOTIFICATIONS'
+my_path = '/home/piyush/Downloads/NITJSR_NOTICES'
 try:
     os.mkdir(my_path)
 except FileExistsError:
     pass
 
 for z in range(0,i):
+    s.call(['notify-send','New Notification',name[z].strip()])
     urllib.request.urlretrieve('http://www.nitjsr.ac.in/'+link[z],path.join(my_path,name[z].strip()))
     st = st + name[z]
 
-with open('.notifications.txt', 'r') as original: data = original.read()
-with open('.notifications.txt', 'w') as modified: modified.write(st + data)
+with open('.notice.txt', 'r') as original: data = original.read()
+with open('.notice.txt', 'w') as modified: modified.write(st + data)
