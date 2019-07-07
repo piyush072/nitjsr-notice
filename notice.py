8bd48206597f13a2c8d4c592daf3ddec09bc4c93
Piyush <2017ugcs007@nitjsr.ac.in>
2019-07-08 02:03:31 +0530

    checks for unread notifications

--- notice.py
+++ notice.py
@@ -1,16 +1,42 @@
-from selenium import webdriver
+import requests
 from bs4 import BeautifulSoup
-
-browser = webdriver.Chrome("/home/piyush/Downloads/chromedriver_linux64/chromedriver") #change path here
+from os import path
+import urllib
+import os
 
 URL = "http://www.nitjsr.ac.in/allnotice.php"
-browser.get(URL)
 
-page = browser.page_source
-soup = BeautifulSoup(page, features='lxml')
+r = requests.get(URL)
 
-for td in soup.find_all('td',attrs={'class':'fsize foo'}):
-    for a in td.find_all('a',attrs={'class':'fsize foo'}):
-        print(a.text)
+page = r.content
+soup = BeautifulSoup(page, features='html5lib')
 
-browser.close()
+name = []
+link = []
+
+for t in soup.find_all('table',attrs={'class':'table table-striped'}):
+    for a in t.find_all('a',href=True):
+        name.append(a.text+'\n')
+        link.append(a['href'].replace(' ','%20'))
+
+if(path.exists(".notifications.txt")):
+    file = open(".notifications.txt", "r")
+    x = file.readline()
+    for i in range(0,10):
+        if(x == name[i]):
+            break
+    print(i)
+st = ""
+my_path = '/home/piyush/Downloads/NITJSR_NOTIFICATIONS'
+try:
+    os.mkdir(my_path)
+except FileExistsError:
+    pass
+
+for z in range(0,i):
+    print('http://www.nitjsr.ac.in/'+link[z].strip())
+    urllib.request.urlretrieve('http://www.nitjsr.ac.in/'+link[z],path.join(my_path,name[z].strip()))
+    st = st + name[z]
+
+with open('.notifications.txt', 'r') as original: data = original.read()
+with open('.notifications.txt', 'w') as modified: modified.write(st + data)
